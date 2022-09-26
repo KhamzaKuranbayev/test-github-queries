4d8a98f79c29352924a26b0639afc2883e363610
KhamzaKuranbayev <khamza@nightwell-logistics.com>
2022-09-26 16:58:01 +0500

    Initial commit

---
+++ src/main/java/Runnable.java
@@ -0,0 +1,10 @@
+/**
+ * Author: khamza@nightwell-logistics.com
+ * Date: 9/26/2022
+ * Time: 4:57 PM
+ */
+public class Runnable {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Author: khamza@nightwell-logistics.com
+ * Date: 9/26/2022
+ * Time: 4:57 PM
+ */
+public class User {
+    private String name;
+}
